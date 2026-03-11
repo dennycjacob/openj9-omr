eclipse-omr/omr

Make retryCodeCacheAllocAndIgnoreKind true by default
Replace the option retryCodeCacheAllocAndIgnoreKind
with a new option that means the opposite. (Essentially
make retryCodeCacheAllocAndIgnoreKind=1 the default
behaviour).

compiler/control/OMROptions.hpp
@@ -439,7 +439,7 @@ enum TR_CompilationOptions {
 
     // Option word 11
     //
-    TR_RetryCodeCacheAllocAndIgnoreKind                      = 0x00000020 + 11,
+    TR_AlwaysObeyCodeCacheKind                      = 0x00000020 + 11,
     TR_DisableVectorAPIBoxing                                = 0x00000040 + 11,
     TR_EnableSelectiveEnterExitHooks                         = 0x00000080 + 11,
     // Available                                             = 0x00000100 + 11,

dennycjacob/openj9-omr

compiler/control/OMROptions.hpp
@@ -439,7 +439,7 @@ enum TR_CompilationOptions {
 
     // Option word 11
     //
-    TR_RetryCodeCacheAllocAndIgnoreKind                      = 0x00000020 + 11,
+    TR_DisableRetryCodeCacheAllocAndIgnoreKind               = 0x00000020 + 11,
     TR_DisableVectorAPIBoxing                                = 0x00000040 + 11,
     TR_EnableSelectiveEnterExitHooks                         = 0x00000080 + 11,
     // Available                                             = 0x00000100 + 11,