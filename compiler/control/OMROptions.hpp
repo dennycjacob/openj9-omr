0xdaryl/omr

Introduce options to force logger choice
Provide options to force use of `OMR::TRIOStreamLogger` and
`OMR::CStdIOStreamLogger` regardless of default logger.

Signed-off-by: Daryl Maier <maier@ca.ibm.com>

compiler/control/OMROptions.hpp
@@ -468,8 +468,8 @@ enum TR_CompilationOptions {
     // Available                                             = 0x80000000 + 11,
 
     // Option word 12
-    // Available                                             = 0x00000020 + 12,
-    // Available                                             = 0x00000040 + 12,
+    TR_ForceCStdIOForLogFile                                 = 0x00000020 + 12,
+    TR_ForceTRIOForLogFile                                   = 0x00000040 + 12,
     TR_DisablePartialInlining                                = 0x00000080 + 12,
     TR_AssumeStartupPhaseUntilToldNotTo                      = 0x00000100 + 12,
     // Available                                             = 0x00000200 + 12,

dennycjacob/openj9-omr

compiler/control/OMROptions.hpp
@@ -468,8 +468,8 @@ enum TR_CompilationOptions {
     // Available                                             = 0x80000000 + 11,
 
     // Option word 12
-    // Available                                             = 0x00000020 + 12,
-    // Available                                             = 0x00000040 + 12,
+    TR_ForceCStdIOForLoggers                                 = 0x00000020 + 12,
+    TR_ForceTRIOForLoggers                                   = 0x00000040 + 12,
     TR_DisablePartialInlining                                = 0x00000080 + 12,
     TR_AssumeStartupPhaseUntilToldNotTo                      = 0x00000100 + 12,
     // Available                                             = 0x00000200 + 12,